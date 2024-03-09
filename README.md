052ea5b35350bce32aa16fb66f1f40d0bf5a00c0
Harold Tomlinson <haroldt@creacom.ca>
2024-03-09 18:34:42 -0500

    About to start 'Adding  acoordinate property'

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-#  <#Title#>
+#  Title
 
+https://learning.oreilly.com/library/view/test-driven-ios-development/9781803232485/B18127_05_ePub.xhtml#:-:text=Adding%20an%20itemDescription%20property
+
+Current:  Chapter 4:  Implementing the ToDoItem struct
+
+Notes:
+- Add '@testable' to test a module by making the protected members public.
--- ToDoTests/ToDoItemTests.swift
+++ ToDoTests/ToDoItemTests.swift
@@ -9,18 +9,40 @@ import XCTest
 @testable import ToDo
 
 final class ToDoItemTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     // ToDoItemTests.swift
-    func test_init_takesTitle() {
-      ToDoItem(title: "Dummy")
+    func test_init_whenGivenTitle_setsTitle()
+    {
+        let item = ToDoItem( title: "Dummy",
+                             itemDescription: "Dummy Description" )
+        XCTAssertNotNil( item.title, "Dummy" )
+        XCTAssertNotNil( item.itemDescription, "Dummy Description" )
     }
-
+    
+    func test_init_setsTimestamp() throws
+    {
+        let dummyTimestamp: TimeInterval = 42.0
+        let item = ToDoItem( title: "Dummy",
+                             timestamp: dummyTimestamp )
+        let timestamp = try XCTUnwrap( item.timestamp )
+        XCTAssertEqual( timestamp, dummyTimestamp,
+                        accuracy: 0.000_001 )
+    }
+    
+    func test_whenGivenLocation_setsLocation()
+    {
+        let dummyLocation = Location( name: "Dummy Name" )
+        let item = ToDoItem( title: "Dummy Title",
+                             location: dummyLocation )
+        XCTAssertEqual( item.location?.name,
+                        dummyLocation.name )
+    }
 }
